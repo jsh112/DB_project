7584f2aef1509c070fb6ae1ff84e81bdb431dc3e
jsh112 <jshkr1120@gmail.com>
2024-11-14 20:56:55 +0900

    test

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     
     LibraryDatabase(db_name="library.db", csv_file="library.csv")
     
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
