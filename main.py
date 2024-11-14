21631484b1578c18a2210dfe897a4d3f924f94bc
Jang se hwan <128944018+jsh112@users.noreply.github.com>
2024-11-14 10:17:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from book_rental_system import LibraryDatabase
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
     # First, create tables and load data from the CSV file
     
