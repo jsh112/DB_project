28d84b604baa5a22bc31114f4d589d6ac551fd9d
상윤 <tlstkddbs223@naver.com>
2024-11-30 22:04:10 +0900

    추천 도서 리스트 만들기

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
-from flask import render_template, request, redirect, url_for, abort
+from flask import render_template, request, redirect, url_for, abort, session
 import sqlite3
+
 
 
 def init_routes(app):
     @app.route('/')
     def home():
-        return render_template('home.html')
+        try:
+            conn = sqlite3.connect('library.db')
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            
+            cursor.execute("SELECT author, title, published_year, ISBN FROM Book LIMIT 10")
+            recommended_books = cursor.fetchall()
+
+        except sqlite3.Error as e:
+            recommended_books = []
+            print(f"Database error: {e}")
+        finally:
+            conn.close()
+
+        return render_template('home.html', recommended_books=recommended_books)
 
     @app.route('/search/', methods=['GET', 'POST'])
     def search():
@@ -55,3 +70,46 @@ def init_routes(app):
     @app.route('/login/')
     def login():
         pass
+
+    # 대출 라우트
+    @app.route('/rent/<isbn>', methods=['POST'])
+    def rent_book(isbn):
+        # 사용자 로그인 체크
+        user_id = session.get('user_id')  # 세션에서 유저 아이디 가져오기
+        if not user_id:
+            return "User not logged in", 401
+        try:
+            conn = sqlite3.connect('library.db')
+            cursor = conn.cursor()
+
+            # Check if the book is available
+            cursor.execute("SELECT available_rent FROM Book WHERE ISBN = ?", (isbn,))
+            result = cursor.fetchone()
+            if result and result[0] == 0:
+                return "This book is not available for rent.", 400
+
+            # Update Book availability
+            cursor.execute("UPDATE Book SET available_rent = 0 WHERE ISBN = ?", (isbn,))
+
+            # Insert into Rental
+            user_id = session.get('user_id')  # Use session for the logged-in user ID
+            if not user_id:
+                return "User not logged in", 401
+
+            rental_date = "2024-11-30"  # Use datetime.now() for the current date
+            due_date = "2024-12-07"  # Example: 7 days from rental_date
+            cursor.execute('''
+                INSERT INTO Rental (RentalDate, Status, Rent_ISBN, User_ID, DueDate)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (rental_date, "Rented", isbn, user_id, due_date))
+
+            conn.commit()
+            return "Book rented successfully!", 200
+
+        except sqlite3.Error as e:
+            return f"Database error: {e}", 500
+        finally:
+            conn.close()
+
+
+   
